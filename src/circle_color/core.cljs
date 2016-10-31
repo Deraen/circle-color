0e69377ad986812eef4e7b9d1c45278c43e06240
Juho Teperi <juho.teperi@iki.fi>
2016-10-31 12:09:00 +0200

    Fix object-assign function call

--- src/circle_color/core.cljs
+++ src/circle_color/core.cljs
@@ -2,5 +2,5 @@
   (:require [object-assign]
             [Circle]))
 
-(js/console.log (js/module$cljsjs_npm$node_modules$object_assign$index #js {:foo 0} #js {:bar 1}))
+(js/console.log (js/object-assign #js {:foo 0} #js {:bar 1}))
 (js/console.log (js/Circle))
